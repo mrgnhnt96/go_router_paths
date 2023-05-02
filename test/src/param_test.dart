87e15c31e1edad5761f3526d7d6f49aaa653c0cd
Morgan <mrgnhnt96@gmail.com>
2023-05-02 08:01:45 -0500

    feat: add tests to check for format queries and throw exceptions

--- test/src/param_test.dart
+++ test/src/param_test.dart
@@ -40,4 +40,29 @@ void main() {
   test('#id returns "oneId"', () {
     expect(Param('all', 'oneId').id, 'oneId');
   });
+
+  test('formats queries', () {
+    expect(
+      Param<Param>('all', 'oneId')
+          .define('abc')
+          .query({'one': '1', 'two': '2'}).path,
+      '/all/abc?one=1&two=2',
+    );
+  });
+
+  group('throws when', () {
+    test('param is not defined', () {
+      expect(
+        () => Param.only('id', parent: Path('some')).path,
+        throwsA(isA<Exception>()),
+      );
+    });
+
+    test('param contains illegal chars', () {
+      expect(
+        () => Param.only('id!', parent: Path('some')).goRoute,
+        throwsA(isA<ArgumentError>()),
+      );
+    });
+  });
 }
